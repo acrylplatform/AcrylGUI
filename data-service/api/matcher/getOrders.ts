2918931fb7e91620eee9f3e3d2eb0b8f935611eb
Egor Zudin <verificatorrus@gmail.com>
2019-01-12 18:36:43 +0700

    Revert "COMISS-1: fix id Asset"

--- data-service/api/matcher/getOrders.ts
+++ data-service/api/matcher/getOrders.ts
@@ -1,3 +1,4 @@
+import { WAVES_ID } from '@waves/signature-generator';
 import { Asset, AssetPair, BigNumber, Money, OrderPrice } from '@waves/data-entities';
 import { IHash, IMoneyFactory, IPriceMoneyFactory } from '../../interface';
 import { coinsMoneyFactory, normalizeAssetId, normalizeTime, priceMoneyFactory, toHash } from '../../utils/utils';
@@ -7,7 +8,7 @@ import { get as configGet } from '../../config';
 import { get as getAsset } from '../assets/assets';
 import { api, IOrder } from './interface';
 
-const WAVES_ID = 'ACRYL';
+
 let signatureData: ISignatureData;
 let timer = null;
 
