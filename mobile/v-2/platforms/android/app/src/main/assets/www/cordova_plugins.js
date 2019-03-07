0fd6f72ef9d7454ff2ac967e32cdfd8894f479a5
PaulK <pk@theinvaders.pro>
2019-03-07 22:26:46 +0200

    MOB-2: change permission camera

--- mobile/v-2/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ mobile/v-2/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -12,8 +12,8 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-qrscanner": "2.6.0"
+  "cordova-plugin-qrscanner": "2.6.0",
+  "cordova-plugin-whitelist": "1.3.3"
 };
 // BOTTOM OF METADATA
 });
