689321fdd98cc93a9dd1c352dbb4fb867dd3e6c9
Paul <spread2009@gmail.com>
2019-06-11 20:21:06 +0300

    COM-3: add several miner tx

--- src/modules/shop/controllers/ShopCtrl.js
+++ src/modules/shop/controllers/ShopCtrl.js
@@ -10,9 +10,9 @@
      * @return {ShopCtrl}
      */
     const controller = function (Base, $scope, modalManager, createPoll, waves) {
-
         const { SIGN_TYPE } = require('@waves/signature-adapter');
         const ds = require('data-service');
+        const { fetch } = require('data-service');
 
         class ShopCtrl extends Base {
 
@@ -49,6 +49,9 @@
              * @type {BigNumber}
              */
             sumOrder = null;
+            sumInMoney = null;
+            sellerData = {};
+            descriptionOrderChunk = '';
             /**
              * Token name
              * @type {string}
@@ -93,43 +96,70 @@
              * @private
              */
             _fee = null;
-            sumInMoney = null;
 
             constructor() {
                 super($scope);
 
-                createPoll(this, this._getBalance, '_balance', 5000, { isBalance: true, $scope });
-
-                this.observe('countOfMiners', this._onChangeCountOfMiners);
-                ds.moneyFromTokens('0.001', WavesApp.defaultAssets.WAVES).then((money) => {
-                    this._fee = money;
-                    $scope.$digest();
+                createPoll(this, this._getBalance, '_balance', 5000, {
+                    isBalance: true,
+                    $scope
                 });
+                this.sellerData = this._getMinerPrice();
+                this.observe('countOfMiners', this._onChangeCountOfMiners);
             }
 
             generate(signable) {
-                return modalManager.showConfirmTx(signable).then(() => this._reset());
+                // console.log('signable._forSign.data.partAttacment :', signable._forSign.data.partAttacment);
+                // console.log('signable._forSign.data.partAttacmen :', this.descriptionOrderChunk[0].length);
+                modalManager.showConfirmTx(signable);
+                // .then(() => {
+                this._processAfterConfirmation(+signable._forSign.data.partAttacment);
+                // });
             }
 
-            createSignable() {
-                const descriptionOrder = `${this.country}${':'}${this.state}${':'}${this.city}
-                ${':'}${this.address}${':'}${this.phone}${':'}${this.email}${':'}
-                ${this.zip}${':'}${this.countOfMiners.toNumber()}`;
+            buyMiner() {
+                this._splitAttachmentString();
+                const partsAttachmentMessage = this.descriptionOrderChunk.length;
+                for (let i = 0; i < partsAttachmentMessage; i++) {
+                    this.createSignable(i);
+                    // console.log('i :', i);
+                }
+            }
 
+            createSignable(indexAttacment) {
+                // console.log('indexAttacment :', this.descriptionOrderChunk[indexAttacment].length);
+                // console.log('this.descriptionOrderChunk.length :', this.descriptionOrderChunk[indexAttacment]);
                 const tx = waves.node.transactions.createTransaction({
                     type: SIGN_TYPE.TRANSFER,
-                    recipient: '3EWsNgTCibx4T5qLwgMhAASRrKdc5syH5Zc',
-                    attachment: descriptionOrder,
-                    amount: this.sumInMoney,
+                    recipient: this.sellerData.sellerAddress,
+                    attachment: this.descriptionOrderChunk[indexAttacment],
+                    amount: (indexAttacment === 0) ? this.sumInMoney : this._fee,
                     fee: this._fee,
-                    shop: true
+                    shop: true,
+                    partAttacment: indexAttacment
                 });
 
                 const signable = ds.signature.getSignatureApi().makeSignable({
                     type: tx.type,
                     data: tx
                 });
-                return signable;
+                this.generate(signable);
+            }
+
+            _processAfterConfirmation(indexAttacment) {
+                if (indexAttacment === (this.descriptionOrderChunk.length - 1)) {
+                    return this._reset();
+                }
+            }
+
+            _splitAttachmentString() {
+                const regexChunk = new RegExp(`.{1,${110}}`, 'g');
+                const userOrder = `${this.country}${':'}${this.state}${':'}
+                    ${this.city}${':'}${this.address}${':'}${this.phone}${':'}${this.nameBuyer}${':'}                
+                    ${this.email}${':'}${this.zip}${':'}${this.countOfMiners.toNumber()}`;
+                this.descriptionOrderChunk = userOrder.match(regexChunk);
+                // console.log('this.descriptionOrderChunk.length :', this.descriptionOrderChunk.length);
+                return this.descriptionOrderChunk;
             }
 
             /**
@@ -140,24 +170,40 @@
                 return waves.node.assets.balance(WavesApp.defaultAssets.WAVES);
             }
 
+            _setFee(fee) {
+                ds.moneyFromTokens(fee, WavesApp.defaultAssets.WAVES)
+                    .then(money => {
+                        this._fee = money;
+                        $scope.$digest();
+                    });
+            }
+
             _getMinerPrice() {
-                return 1 / 13;
+                return fetch(WavesApp.network.shop)
+                    .then((resp) => {
+                        this.sellerData = JSON.parse(resp);
+                        this._setFee(this.sellerData.fee);
+                        return this.sellerData;
+                    });
             }
 
             _onChangeCountOfMiners({ value }) {
                 const clearBalance = this._balance.available.getTokens();
-                const sum = value ? value.times(this._getMinerPrice()) : 0;
+                // console.log('this.sellerData :', this.sellerData);
+                const sum = value ? value.times(+this.sellerData.priceMiner) : 0;
                 if (value && value.toNumber() && clearBalance.gt(sum)) {
                     this.sumOrder = sum;
                 } else {
                     this.sumOrder = null;
                     this.countOfMiners = null;
                 }
-                const sumInNumber = (this.sumOrder) ? this.sumOrder.toNumber() : 0;
-                ds.moneyFromTokens(sumInNumber, WavesApp.defaultAssets.WAVES).then((money) => {
-                    this.sumInMoney = money;
-                    $scope.$digest();
-                });
+                const sumInNumber = this.sumOrder ? this.sumOrder.toNumber() : 0;
+                ds
+                    .moneyFromTokens(sumInNumber, WavesApp.defaultAssets.WAVES)
+                    .then(money => {
+                        this.sumInMoney = money;
+                        $scope.$digest();
+                    });
             }
 
             /**
@@ -165,7 +211,9 @@
              * @private
              */
             _onChangeBalance() {
-                this.invalid = (!this._fee || !this._balance) ||
+                this.invalid =
+                    !this._fee ||
+                    !this._balance ||
                     this._balance.available.getTokens().lt(this._fee.getTokens());
             }
 
@@ -184,12 +232,16 @@
             }
 
         }
-
         return new ShopCtrl();
     };
 
-    controller.$inject = ['Base', '$scope', 'modalManager', 'createPoll', 'waves'];
+    controller.$inject = [
+        'Base',
+        '$scope',
+        'modalManager',
+        'createPoll',
+        'waves'
+    ];
 
-    angular.module('app.shop')
-        .controller('ShopCtrl', controller);
+    angular.module('app.shop').controller('ShopCtrl', controller);
 })();
