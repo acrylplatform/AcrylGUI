ef7656673adc99a6a94dbd397481f24f4d828833
Egor Zudin <verificatorrus@gmail.com>
2019-08-14 16:13:32 +0700

    Revert "Update version to 1.0.14"

--- src/modules/app/initialize/AppRun.js
+++ src/modules/app/initialize/AppRun.js
@@ -256,10 +256,6 @@
                         return null;
                     }
 
-                    promise.then(() => {
-                        this._checkReferers();
-                    });
-
                     if (needShowTutorial && toState.name !== 'dex-demo') {
                         modalManager.showTutorialModals();
                         needShowTutorial = false;
