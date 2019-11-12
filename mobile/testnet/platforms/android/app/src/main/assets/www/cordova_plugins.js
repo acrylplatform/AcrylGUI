cc55dbb5fd4edd6986c950bff1f33b5c840924ef
vseriousv <gavrilovskirill@gmail.com>
2019-11-12 12:25:25 +0700

    MOBILE-2: Add version mainnet and testnet

---
+++ mobile/testnet/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,16 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+      "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+      "pluginId": "phonegap-plugin-barcodescanner",
+      "clobbers": [
+        "cordova.plugins.barcodeScanner"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-whitelist": "1.3.4",
+    "phonegap-plugin-barcodescanner": "8.1.0"
+  };
+});
